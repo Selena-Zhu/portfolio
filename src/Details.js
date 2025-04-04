8d1bd0efb16764fb17f9039dba948b92063703b2
Ningning Zhu <ningningzhu@NingningdeMacBook-Pro-2.local>
2025-04-03 17:11:53 -0700

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,203 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Selena Zhu",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I'm a software engineer — and also a knitter and crocheter — who loves using creativity to build thoughtful, user-friendly websites. I bring the same attention to detail and love for design from my crafting into every project I code.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/selena-zhu",
+  github: "https://github.com/Selena-Zhu",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Software Engineer",
+    Company: `Vibrant Wellness`,
+    Location: "California",
+    Type: "Full Time",
+    Duration: "June 2020 - Present",
+  },
+  {
+    Position: "Full Stack Engineer",
+    Company: `Beshton`,
+    Location: "California",
+    Type: "Full Time",
+    Duration: "Nov 2021 - June 2022",
+  },
+  {
+    Position: "Data Analyst",
+    Company: `Lifestyle Agency`,
+    Location: "New York",
+    Type: "Internship",
+    Duration: "Dec 2020 - May 2021",
+  },
+  {
+    Position: "Machine Learning Lab Assistant",
+    Company: `Rice University`,
+    Location: "Texas",
+    Type: "Internship",
+    Duration: "June 2020 - Dec 2020",
+  },
+  {
+    Position: "Machine Learning Teaching Assistant",
+    Company: `Rice University`,
+    Location: "Texas",
+    Type: "Internship",
+    Duration: "June 2020 - Dec 2020",
+  },
+  {
+    Position: "Data Scientist",
+    Company: `Bank of Hangzhou`,
+    Location: "China",
+    Type: "Internship",
+    Duration: "Dec 2018 - May 2019",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Computer Science",
+    Company: "Trine University",
+    Location: "Arizona",
+    Type: "Full Time",
+    Duration: "June 2023 - May 2024",
+  },
+  {
+    Position: "Statistics",
+    Company: `Rice University`,
+    Location: "Texas",
+    Type: "Full Time",
+    Duration: "Aug 2019 - Dec 2020",
+  },
+  {
+    Position: "Statistics",
+    Company: `University of Connecticut`,
+    Location: "Connecticut",
+    Type: "Full Time",
+    Duration: "Aug 2015 - May 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "selenazhu022@gmail.com",
+  phone: "+1 860-786-8790",
+};
